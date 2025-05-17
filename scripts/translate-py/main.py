34f7f2ce9d20b3d81bde09693cb52a500962f334
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2025-05-17 23:22:19 +0200

    feat: :art: add typer & main & test imports

--- scripts/translate-py/main.py
+++ scripts/translate-py/main.py
@@ -1,7 +1,18 @@
 import typer
+from api_client import app as test_connection
 
-def main(name: str):
-    print(f"Hello {name}")
+app = typer.Typer()
+
+@app.command()
+def translate_all(src: str = "docs/en", out: str = "docs"):
+    """
+    Start the translation of every files Mardown.
+    """
+    typer.echo("🚀 Démarrage du processus de traduction")
+    test_connection()
+
+def main():
+    print(f"Hello")
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
